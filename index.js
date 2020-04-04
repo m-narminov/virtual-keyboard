91e8eeeaca06af1e6edea0c041a7564b22b91e27
m.narminov <m.narminov@gmail.com>
2020-04-04 15:10:17 +0300

    feat: add eslint, add template keys

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-<!DOCTYPE html>
+const rows = [
+  ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+  ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+  ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+  ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+  ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'],
+];
+
+window.onload = function () {
+  const root = this.document.createElement('div');
+  root.style.textAlign = 'center';
+
+  const textArea = document.createElement('textarea');
+  textArea.value = 'asdfasdf';
+
+  const keyboard = this.document.createElement('div');
+  for (let i = 0; i < rows.length; i++) {
+    const row = this.document.createElement('div');
+    for (let j = 0; j < rows[i].length; j++) {
+      const key = this.document.createElement('button');
+      key.innerHTML = 'a';
+      row.append(key);
+    }
+    keyboard.append(row);
+  }
+
+  root.append(textArea, keyboard);
+
+  document.body.append(root);
+};
