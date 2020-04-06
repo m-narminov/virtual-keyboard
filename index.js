1a4f1d893da2306f120d7c2b26182ab598f2459c
m.narminov <m.narminov@gmail.com>
2020-04-06 16:55:33 +0300

    fix: add key values

--- index.js
+++ index.js
@@ -60,19 +60,19 @@ const rows = {
       { id: 'Comma', value: 'Б' },
       { id: 'Period', value: 'Ю' },
       { id: 'Slash', value: '.' },
-      { id: 'ArrowUp', value: '⮝' }, // ⮜⮞⮟
+      { id: 'ArrowUp', value: '⮝' },
       { id: 'ShiftRight', value: 'Shift' },
     ],
     [
+      { id: 'ControlLeft', value: 'Ctrl' },
+      { id: 'WindowsKey', value: 'Win' },
+      { id: 'AltLeft', value: 'Alt' },
+      { id: 'Space', value: '' },
+      { id: 'AltRight', value: 'Alt' },
       { id: 'ControlRight', value: 'Ctrl' },
-      { id: 'WindowsKey' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
+      { id: 'ArrowLeft', value: '⮜' },
+      { id: 'ArrowDown', value: '⮟' },
+      { id: 'ArrowRight', value: '⮞' },
     ],
   ],
 
@@ -94,121 +94,124 @@ const rows = {
       { id: 'Backspace', value: 'Backspace' },
     ],
     [
+      { id: 'Tab', value: 'Tab' },
+      { id: 'KeyQ', value: 'Q' },
+      { id: 'KeyW', value: 'W' },
+      { id: 'KeyE', value: 'E' },
+      { id: 'KeyR', value: 'R' },
+      { id: 'KeyT', value: 'T' },
+      { id: 'KeyY', value: 'Y' },
+      { id: 'KeyU', value: 'U' },
+      { id: 'KeyI', value: 'I' },
+      { id: 'KeyO', value: 'O' },
+      { id: 'KeyP', value: 'P' },
+      { id: 'BracketLeft', value: '[' },
+      { id: 'BracketRight', value: ']' },
+      { id: 'Backslash', value: '<sup>/</sup>\\' },
+      { id: 'Delete', value: 'DEL' },
+    ],
+    [
+      { id: 'CapsLock', value: 'CapsLock' },
+      { id: 'KeyA', value: 'A' },
+      { id: 'KeyS', value: 'S' },
+      { id: 'KeyD', value: 'D' },
+      { id: 'KeyF', value: 'F' },
+      { id: 'KeyG', value: 'G' },
+      { id: 'KeyH', value: 'H' },
+      { id: 'KeyJ', value: 'J' },
+      { id: 'KeyK', value: 'K' },
+      { id: 'KeyL', value: 'L' },
+      { id: 'Semicolon', value: ';' },
+      { id: 'Quote', value: "'" },
+      { id: 'Enter', value: 'Enter' },
+    ],
+    [
+      { id: 'ShiftLeft', value: 'Shift' },
+      { id: 'KeyZ', value: 'Z' },
+      { id: 'KeyX', value: 'X' },
+      { id: 'KeyC', value: 'C' },
+      { id: 'KeyV', value: 'V' },
+      { id: 'KeyB', value: 'B' },
+      { id: 'KeyN', value: 'N' },
+      { id: 'KeyM', value: 'M' },
+      { id: 'Comma', value: ',' },
+      { id: 'Period', value: '.' },
+      { id: 'Slash', value: '/' },
+      { id: 'ArrowUp', value: '⮝' },
+      { id: 'ShiftRight', value: 'Shift' },
+    ],
+    [
       { id: 'ControlLeft', value: 'Ctrl' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-    ],
-    [
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-    ],
-    [
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-    ],
-    [
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
-      { id: 'KeyF' },
+      { id: 'WindowsKey', value: 'Win' },
+      { id: 'AltLeft', value: 'Alt' },
+      { id: 'Space', value: '' },
+      { id: 'AltRight', value: 'Alt' },
+      { id: 'ControlRight', value: 'Ctrl' },
+      { id: 'ArrowLeft', value: '⮜' },
+      { id: 'ArrowDown', value: '⮟' },
+      { id: 'ArrowRight', value: '⮞' },
     ],
   ],
-};
+}
 
 class Key {
+  style = {}
+
   constructor() {}
 
-  onClick = () => {};
+  onClick = () => {}
 
-  onkeydown = () => {};
+  onkeydown = () => {}
 
-  onkeyup = () => {};
+  onkeyup = () => {}
 }
 
 window.onload = function () {
-  const meta = this.document.createElement('meta');
-  meta.setAttribute('charset', 'UTF-8');
-  this.console.log(meta);
+  const meta = this.document.createElement('meta')
+  meta.setAttribute('charset', 'UTF-8')
+  this.console.log(meta)
 
-  this.document.head.title.value = 'Virtual keyboard';
-  let eng = false;
-  const keyboardPress = (e) => {
-    textArea.value += e.code;
-    this.console.log('e.key = ', e.key);
-    this.console.log('e.code = ', e.code);
+  this.document.append(
+    (this.document.createElement('title').innerHTML = 'Virtual keyboard')
+  )
+  let eng = false
+  const keyboardPress = e => {
+    textArea.value += e.code
+    this.console.log('e.key = ', e.key)
+    this.console.log('e.code = ', e.code)
     if (e.key === 'Shift' && e.key === 'Alt') {
-      eng = !eng;
+      eng = !eng
     }
-  };
-
-  // window.onkeydown = window.onkeyup =
-  window.onkeypress = keyboardPress;
-  const root = this.document.createElement('div');
-  root.style.textAlign = 'center';
-
-  const textArea = document.createElement('textarea');
-  textArea.value = '';
-  textArea.style.width = '100%';
-  textArea.style.height = '300px';
-
-  const keyboard = this.document.createElement('div');
-
-  const lang = eng ? 'en' : 'ru';
-  this.console.log(lang);
-
-  for (let i = 0; i < rows[lang].length; i++) {
-    const row = this.document.createElement('div');
-    for (let j = 0; j < rows[lang][i].length; j++) {
-      const key = this.document.createElement('button');
-      key.name = rows[lang][i][j].id;
-      key.innerHTML = rows[lang][i][j].value;
-      key.addEventListener('click', (e) => {});
-      row.append(key);
-    }
-    keyboard.append(row);
   }
 
-  root.append(textArea, keyboard);
+  // window.onkeydown = window.onkeyup =
+  window.onkeypress = keyboardPress
+  const root = this.document.createElement('div')
+  root.style.textAlign = 'center'
 
-  document.body.append(root);
-};
+  const textArea = document.createElement('textarea')
+  textArea.value = ''
+  textArea.style.width = '100%'
+  textArea.style.height = '300px'
+
+  const keyboard = this.document.createElement('div')
+
+  const lang = eng ? 'en' : 'ru'
+  this.console.log(lang)
+
+  for (let i = 0; i < rows[lang].length; i++) {
+    const row = this.document.createElement('div')
+    for (let j = 0; j < rows[lang][i].length; j++) {
+      const key = this.document.createElement('button')
+      key.name = rows[lang][i][j].id
+      key.innerHTML = rows[lang][i][j].value
+      key.addEventListener('click', e => {})
+      row.append(key)
+    }
+    keyboard.append(row)
+  }
+
+  root.append(textArea, keyboard)
+
+  document.body.append(root)
+}
